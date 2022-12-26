7bb4b8870428055f0e0de126112c1ebc22ce7aff
Muhammad Basari <107491771+Basari-ID@users.noreply.github.com>
2022-12-27 01:37:56 +0700

    Update lw.py

--- lw.py
+++ lw.py
@@ -1,87 +1,96 @@
-import requests,bs4,json,os,sys,random,datetime,time,re
-import threading
-from rich.table import Table as me
-from rich.console import Console as sol
-from bs4 import BeautifulSoup as parser
-from concurrent.futures import ThreadPoolExecutor as tred
-from rich.console import Group as gp
-from rich.panel import Panel as panel
-from rich import print as cetak
-from rich.markdown import Markdown as mark
-from rich.columns import Columns as col
-try:ugen = open('user.txt','r').read().splitlines()
-except:ugen = ['Mozilla/5.0 (Linux; U; Android 2.3.4; pt-pt; SonyEricssonLT18a Build/4.0.1.A.0.266) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1','Mozilla/5.0 (Linux; U; Android 4.2.1; ru-ru; 9930i Build/JOP40D) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30','Mozilla/5.0 (Linux; U; Android 2.3.4; ru-ru; MID Build/GRJ22) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1','Mozilla/5.0 (Linux; U; Android 4.3; en-us; ASUS_T00J Build/JSS15Q) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30','Mozilla/5.0 (Linux; U; Android 4.2.2; ru-ru; Fly IQ4404 Build/JDQ39) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30 YandexSearch/7.16']
-try:ugen2 = open('user2.txt','r').read().splitlines()
-except:ugen2 = ['Mozilla/5.0 (Linux; U; Android 2.3.4; pt-pt; SonyEricssonLT18a Build/4.0.1.A.0.266) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1','Mozilla/5.0 (Linux; U; Android 4.2.1; ru-ru; 9930i Build/JOP40D) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30','Mozilla/5.0 (Linux; U; Android 2.3.4; ru-ru; MID Build/GRJ22) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1','Mozilla/5.0 (Linux; U; Android 4.3; en-us; ASUS_T00J Build/JSS15Q) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30','Mozilla/5.0 (Linux; U; Android 4.2.2; ru-ru; Fly IQ4404 Build/JDQ39) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30 YandexSearch/7.16']
-pwtambahanya = []
-pwtambahan = []
-oke,cepe = 0,0
-method = []
-basid1 = []
-basid2 = []
-loop = 0
-akun = []
-idmu = []
-#
-P = '\x1b[1;97m' 
-M = '\x1b[1;91m' 
-H = '\x1b[1;92m' 
-K = '\x1b[1;93m' 
-B = '\x1b[1;94m' 
-U = '\x1b[1;95m' 
-O = '\x1b[1;96m' 
-N = '\x1b[0m'    
-Z = "\033[1;30m"
-x = '\33[m' 
-m = '\x1b[1;91m' 
-k = '\033[93m' 
-h = '\x1b[1;92m' 
-hh = '\033[32m' 
-u = '\033[95m' 
-kk = '\033[33m' 
-b = '\33[1;96m' 
-p = '\x1b[0;34m' 
-#
-dic = {'1':'Januari','2':'Februari','3':'Maret','4':'April','5':'Mei','6':'Juni','7':'Juli','8':'Agustus','9':'September','10':'Oktober','11':'November','12':'Desember'}
-dic2 = {'01':'Januari','02':'Februari','03':'Maret','04':'April','05':'Mei','06':'Juni','07':'Juli','08':'Agustus','09':'September','10':'Oktober','11':'November','12':'Desember'}
-tgl = datetime.datetime.now().day
-bln = dic[(str(datetime.datetime.now().month))]
-thn = datetime.datetime.now().year
-okc = 'OK-'+str(tgl)+'-'+str(bln)+'-'+str(thn)+'.txt'
-cpc = 'CP-'+str(tgl)+'-'+str(bln)+'-'+str(thn)+'.txt'
-#
-def basari(basjalan):
-        for e in basjalan + "\n":sys.stdout.write(e);sys.stdout.flush();time.sleep(0.05)
-#
-def kembali():
-	login()
-#
-def clearlayar():
-	if "linux" in sys.platform.lower():
-		try:os.system("clear")
-		except:pass
-	elif "win" in sys.platform.lower():
-		try:os.system("cls")
-		except:pass
+import os,sys,requests,re
+
+def note():
+	print('''-pembuat arvel ry ganteng slemek slebew Sebelum Menggunakan Tools Ini Taro Tools Ini Dengan
+folder Bersamaan Dengan Sc Kalian\n''')
+
+def menu():
+	note()
+	print('1. File Token .token.txt & .cok.txt')
+	print('2. File Token t.txt & c.txt')
+	print('3. File Token token.txt & cok.txt')
+	menu = input('\npilih : ')
+	if menu in ['1']:
+		os.system('rm -rf .token.txt')
+		os.system('rm -rf .cok.txt')
+		token1()
+	elif menu in ['2']:
+		os.system('rm -rf t.txt')
+		os.system('rm -rf c.txt')
+		token2()
+	elif menu in ['3']:
+		os.system('rm -rf token.txt')
+		os.system('rm -rf cok.txt')
+		token3()
 	else:
-		try:os.system("clear")
-		except:pass
-#
-try:
-	bas_wrn = "#0000FF"
-except FileNotFoundError:
-	bas_wrn = "#0000FF"
-#
-cetak(panel(f'''
-{m}  ____    _    ____    _    ____  ___ 
-{m} | __ )  / \  / ___|  / \  |  _ \|_ _|
-{m} |  _ \ / _ \ \___ \ / _ \ | |_) || | 
-{x} | |_) / ___ \ ___) / ___ \|  _ < | | 
-{x} |____/_/   \_\____/_/   \_\_| \_\___|
-  ''',style=f'{bas_wrn}',title=f''))
-#
-cetak(panel(f'''{x}1. crack publik massal
-{x}2. crack dari followers
-{x}3. crack dari grup fb
-{x}4. chek hasil crack
-{x}5. keluar hapus coki''',title=f'[cyan] menu',style=f'{bas_wrn}'))
+		exit()
+
+def token1():
+	try:
+		ses = requests.Session()
+		cookie = input('\nMasukan Cookie : ')
+		cookies = {'cookie':cookie}
+		url = 'https://www.facebook.com/adsmanager/manage/campaigns'
+		req = ses.get(url,cookies=cookies)
+		set = re.search('act=(.*?)&nav_source',str(req.content)).group(1)
+		nek = '%s?act=%s&nav_source=no_referrer'%(url,set)
+		roq = ses.get(nek,cookies=cookies)
+		tok = re.search('accessToken="(.*?)"',str(roq.content)).group(1)
+		tokenw = open(".token.txt", "w").write(tok)
+		cokiew = open(".cok.txt", "w").write(cookie)
+		print('\nLogin Berhasil , file tersimpan di .token.txt & .cok.txt')
+	except Exception as e:
+		os.system('rm -rf .cok.txt && rm -rf .token.txt')
+		print(e)
+		exit()
+
+
+def token2():
+	try:
+		ses = requests.Session()
+		print('Login Menggunakan Cookie')
+		print('Pastikan Jangan Menggunakan Akun Utama !')
+		cookie = input('\nMasukan Cookie : ')
+		cookies = {'cookie':cookie}
+		url = 'https://www.facebook.com/adsmanager/manage/campaigns'
+		req = ses.get(url,cookies=cookies)
+		set = re.search('act=(.*?)&nav_source',str(req.content)).group(1)
+		nek = '%s?act=%s&nav_source=no_referrer'%(url,set)
+		roq = ses.get(nek,cookies=cookies)
+		tok = re.search('accessToken="(.*?)"',str(roq.content)).group(1)
+		tokenw = open("t.txt", "w").write(tok)
+		cokiew = open("c.txt", "w").write(cookie)
+		print('\nLogin Berhasil , file tersimpan di t.txt & c.txt')
+	except Exception as e:
+		os.system('rm -rf c.txt && rm -rf t.txt')
+		print(e)
+		exit()
+
+def token3():
+	try:
+		ses = requests.Session()
+		print('Login Menggunakan Cookie')
+		print('Pastikan Jangan Menggunakan Akun Utama !')
+		cookie = input('\nMasukan Cookie : ')
+		cookies = {'cookie':cookie}
+		url = 'https://www.facebook.com/adsmanager/manage/campaigns'
+		req = ses.get(url,cookies=cookies)
+		set = re.search('act=(.*?)&nav_source',str(req.content)).group(1)
+		nek = '%s?act=%s&nav_source=no_referrer'%(url,set)
+		roq = ses.get(nek,cookies=cookies)
+		tok = re.search('accessToken="(.*?)"',str(roq.content)).group(1)
+		tokenw = open("token.txt", "w").write(tok)
+		cokiew = open("cok.txt", "w").write(cookie)
+		print('\nLogin Berhasil , file tersimpan di token.txt & cok.txt')
+	except Exception as e:
+		os.system('rm -rf cok.txt && rm -rf token.txt')
+		print(e)
+		exit()
+
+if __name__=='__main__':
+	try:os.mkdir('OK')
+	except:pass
+	try:os.mkdir('CP')
+	except:pass
+	os.system('clear')
+	menu()
